9237b22d1b82901f90f85d7b34036eb1539b4396
Imvajeed <iddalagivajeed@gmail.com>
2024-06-03 20:12:02 +0530

    getting items and handling clickes second time

--- app/src/main/java/com/example/oneclickdriveinterview/MainActivity.kt
+++ app/src/main/java/com/example/oneclickdriveinterview/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.oneclickdriveinterview
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.oneclickdriveinterview.databinding.ActivityMainBinding
+import com.example.oneclickdriveinterview.viewmodel.MainViewModel
+import com.example.oneclickdriveinterview.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var viewModelFactory: MainViewModelFactory
+    lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.apply {
+            calculateBtn.setOnClickListener{
+                getData()
+            }
+        }
+
+    }
+
+    private fun getData() {
+        binding.apply {
+            val text1 = textBox1.text.toString()
+            val text2 = textBox2.text.toString()
+            val text3 = textBox3.text.toString()
+
+            if (!text1.isBlank() && !text2.isBlank() && !text3.isBlank()){
+                var arr1 = text1.split(",")
+                var arr2 = text2.split(",")
+                var arr3 = text3.split(",")
+                Log.i("textOne","$arr1 $arr2 $arr3")
+
+                viewModelFactory = MainViewModelFactory(arr1,arr2,arr3)
+
+
+            }else{
+                Toast.makeText(
+                    this@MainActivity,
+                    "Must fill all the Fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/oneclickdriveinterview/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/oneclickdriveinterview/viewmodel/MainViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.oneclickdriveinterview.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModel(val set1 : List<String>,val set2 : List<String>,val set3 : List<String>):ViewModel() {
+
+}
+
+class MainViewModelFactory(val set1 : List<String>,val set2 : List<String>,val set3 : List<String>):ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(set1, set2,set3) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
